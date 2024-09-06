9bb765c8716a43de985fa5e5fb934310c2ba2bae
mdelgado96 <d.marvin96@gmail.com>
2024-09-05 20:42:53 -0700

    Started on the Nav

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-/* This is an example JavaScript file, feel free to remove/edit it anytime */
-console.log(
-  "%cProject by BigDevSoon",
-  'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
-);
-console.log("Check out more projects at https://bigdevsoon.me");
+const api = 'https://dummyjson.com/users'
+
+fetch('https://dummyjson.com/users')
+.then(res => res.json())
+.then(console.log);
