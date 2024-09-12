cd4a72ffec797e3f9417ddf43a808113b9d5dbe8
mdelgado96 <d.marvin96@gmail.com>
2024-09-11 20:15:16 -0700

    Minor changes

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const api = 'https://dummyjson.com/users'
 fetch('https://dummyjson.com/users')
 .then(res => res.json())
 .then(console.log);
+
+function closeNav() {
+    document.getElementById("nav_column_open").remove;
+    document.append("");
+}
