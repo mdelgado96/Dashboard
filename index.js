e856abc1532c8515b83fd5cd2ea4fd89142fe46a
mdelgado96 <d.marvin96@gmail.com>
2024-09-29 15:18:02 -0700

    working on fixing the append function.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const api = 'https://dummyjson.com/users';
+const api = "https://dummyjson.com/users";
 const userIncomeTemplate = document.querySelector("[data-income-template]");
 const userProfitTemplate = document.querySelector("[data-profit-template]");
 const userViewsTemplate = document.querySelector("[data-views-template]");
@@ -6,27 +6,27 @@ const userRateTemplate = document.querySelector("[data-rate-template]");
 const userCardContainer = document.querySelector("[data-user-cards-container]");
 
 function openNav() {
-    document.getElementById("side_bar").classList.remove('nav_column_closed');
-    document.getElementById("side_bar").classList.add('nav_column_open');
-    document.getElementById("sidebar_close").classList.add('none');
-    document.getElementById("sidebar_open").classList.remove('none');
-    document.getElementById("sidebar_icon_close").classList.add('none');
-    document.getElementById("sidebar_icon_open").classList.remove('none');
+  document.getElementById("side_bar").classList.remove("nav_column_closed");
+  document.getElementById("side_bar").classList.add("nav_column_open");
+  document.getElementById("sidebar_close").classList.add("none");
+  document.getElementById("sidebar_open").classList.remove("none");
+  document.getElementById("sidebar_icon_close").classList.add("none");
+  document.getElementById("sidebar_icon_open").classList.remove("none");
 }
 
 function closeNav() {
-    document.getElementById("side_bar").classList.remove('nav_column_open');
-    document.getElementById("side_bar").classList.add('nav_column_closed');
-    document.getElementById("sidebar_open").classList.add('none');
-    document.getElementById("sidebar_close").classList.remove('none');
-    document.getElementById("sidebar_icon_open").classList.add('none');
-    document.getElementById("sidebar_icon_close").classList.remove('none');
+  document.getElementById("side_bar").classList.remove("nav_column_open");
+  document.getElementById("side_bar").classList.add("nav_column_closed");
+  document.getElementById("sidebar_open").classList.add("none");
+  document.getElementById("sidebar_close").classList.remove("none");
+  document.getElementById("sidebar_icon_open").classList.add("none");
+  document.getElementById("sidebar_icon_close").classList.remove("none");
 }
 
-fetch('https://dummyjson.com/users/1')
-.then(res => res.json())
-.then(data => {
-    const incomeCard = userIncomeTemplate.content.cloneNode(true);
+fetch("https://dummyjson.com/users/1")
+  .then((res) => res.json())
+  .then((data) => {
+    const incomeCard = userIncomeTemplate.content.cloneNode(true).children[0];
     const profitCard = userProfitTemplate.content.cloneNode(true).children[0];
     const viewCard = userViewsTemplate.content.cloneNode(true).children[0];
     const rateCard = userRateTemplate.content.cloneNode(true).children[0];
@@ -41,11 +41,15 @@ fetch('https://dummyjson.com/users/1')
     view.textContent = data.lastName;
     rate.textContent = data.maidenName;
 
-    userCardContainer.append(income);
+    const test = userIncomeTemplate.appendChild(income);
+    // needs to append inside eache template instead of to the overall 
+    userIncomeTemplate.appendChild(income)
     userCardContainer.append(profit);
     userCardContainer.append(view);
     userCardContainer.append(rate);
 
+    // console.log(userCardContainer.append(income));
+    console.log(test);
     console.log(data);
     console.log(incomeCard);
-});
+  });
