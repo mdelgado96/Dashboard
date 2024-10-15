e791a67f12af07d1a68ab1d31c930a6bd6eef15b
mdelgado96 <d.marvin96@gmail.com>
2024-10-14 19:37:39 -0700

    Got the proper data to show from API.

--- index.js
+++ index.js
@@ -26,32 +26,23 @@ function closeNav() {
 fetch("https://dummyjson.com/users/1")
   .then((res) => res.json())
   .then((data) => {
-    console.log(data);
     const incomeCard = userIncomeTemplate.content.cloneNode(true).children[0]; 
     const profitCard = userProfitTemplate.content.cloneNode(true).children[0];
     const viewCard = userViewsTemplate.content.cloneNode(true).children[0];
     const rateCard = userRateTemplate.content.cloneNode(true).children[0];
-
-    console.log(incomeCard);
 
     const income = incomeCard.querySelector("[data-income-num]");
     const profit = profitCard.querySelector("[data-profit-num]");
     const view = viewCard.querySelector("[data-view-num]");
     const rate = rateCard.querySelector("[data-rate-num]");
 
-    console.log(income);
-
-    income.children = data.height;
+    income.textContent = data.height;
     profit.textContent = data.age;
     view.textContent = data.lastName;
     rate.textContent = data.maidenName;
 
-    userCardContainer.getElementsByClassName("gcn_text").textContent = income;
-    // must append the whole card and not just one line
-    // still searching this issue
-    userCardContainer.append(profit); // 28
-    userCardContainer.append(view);   // Johnson
-    userCardContainer.append(rate);   // Smith
-
-    console.log(incomeCard);
+    userCardContainer.append(incomeCard);
+    userCardContainer.append(profitCard);
+    userCardContainer.append(viewCard);
+    userCardContainer.append(rateCard);
   });
